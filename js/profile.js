8c339114818d6b3e8e02b7eba980680d876601c4
jiseungjung <roadrunner23@yonsei.ac.kr>
2020-06-24 13:54:35 +0900

    js and index url fixes

--- js/profile.js
+++ js/profile.js
@@ -1,81 +1,81 @@
 //PHD
-$('#SCH').on('click', function() {
-    $('#phdPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/SCH_pic.jpg');
-    // $('#phdPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/SCH_pic.jpg');
-    $('#phdContent>h1').html('Cunghyun Son');
-})
+$('#SCH').on('click', function () {
+  $('#phdPic').attr('src', 'images/people/SCH_pic.jpg');
+  // $('#phdPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/SCH_pic.jpg');
+  $('#phdContent>h1').html('Cunghyun Son');
+});
 
-$('#KSG').on('click', function() {
-    $('#phdPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/KSG_pic.jpg');
-    // $('#phdPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KSG_pic.jpg');
-    $('#phdContent>h1').html('Seulgee Kim');
-})
+$('#KSG').on('click', function () {
+  $('#phdPic').attr('src', 'images/people/KSG_pic.jpg');
+  // $('#phdPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KSG_pic.jpg');
+  $('#phdContent>h1').html('Seulgee Kim');
+});
 
-$('#KBM').on('click', function() {
-    $('#phdPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/KBM_pic.jpg');
-    // $('#phdPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KBM_pic.jpg');
-    $('#phdContent>h1').html('Byungmun Kang');
-})
+$('#KBM').on('click', function () {
+  $('#phdPic').attr('src', 'images/people/KBM_pic.jpg');
+  // $('#phdPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KBM_pic.jpg');
+  $('#phdContent>h1').html('Byungmun Kang');
+});
 
 //MS
-$('#BEH').on('click', function() {
-    $('#msPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/BEH_pic.jpg');
-    // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/BEH_pic.jpg');
-    $('#msContent>h1').html('Euihyun Baek');
-})
+$('#BEH').on('click', function () {
+  $('#msPic').attr('src', 'images/people/BEH_pic.jpg');
+  // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/BEH_pic.jpg');
+  $('#msContent>h1').html('Euihyun Baek');
+});
 
-$('#KMC').on('click', function() {
-    $('#msPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/KMC_pic.jpg');
-    // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KMC_pic.jpg');
-    $('#msContent>h1').html('Minchul Kim');
-})
+$('#KMC').on('click', function () {
+  $('#msPic').attr('src', 'images/people/KMC_pic.jpg');
+  // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KMC_pic.jpg');
+  $('#msContent>h1').html('Minchul Kim');
+});
 
-$('#KMD').on('click', function() {
-    $('#msPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/KMD_pic.jpg');
-    // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KMD_pic.jpg');
-    $('#msContent>h1').html('Mandong Kim');
-})
+$('#KMD').on('click', function () {
+  $('#msPic').attr('src', 'images/people/KMD_pic.jpg');
+  // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/KMD_pic.jpg');
+  $('#msContent>h1').html('Mandong Kim');
+});
 
-$('#CJH').on('click', function() {
-    $('#msPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/CJH_pic.jpg');
-    // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/CJH_pic.jpg');
-    $('#msContent>h1').html('Jongha Choi');
-})
+$('#CJH').on('click', function () {
+  $('#msPic').attr('src', 'images/people/CJH_pic.jpg');
+  // $('#msPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/CJH_pic.jpg');
+  $('#msContent>h1').html('Jongha Choi');
+});
 
 //Intern
-$('#JJW').on('click', function() {
-    $('#internPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/JJW_pic.jpg');
-    // $('#internPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/JJW_pic.jpg');
-    $('#internContent>h1').html('Jaewoo Jung');
-})
+$('#JJW').on('click', function () {
+  $('#internPic').attr('src', 'images/people/JJW_pic.jpg');
+  // $('#internPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/JJW_pic.jpg');
+  $('#internContent>h1').html('Jaewoo Jung');
+});
 
-$('#RHS').on('click', function() {
-    $('#internPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/RHS_pic.jpg');
-    // $('#internPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/RHS_pic.jpg');
-    $('#internContent>h1').html('Hyesung Roh');
-})
+$('#RHS').on('click', function () {
+  $('#internPic').attr('src', 'images/people/RHS_pic.jpg');
+  // $('#internPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/RHS_pic.jpg');
+  $('#internContent>h1').html('Hyesung Roh');
+});
 
-$('#JJS').on('click', function() {
-    $('#internPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/JJS_pic.jpg');
-    // $('#internPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/JJS_pic.jpg');
-    $('#internContent>h1').html('Jiseung Jung');
-})
+$('#JJS').on('click', function () {
+  $('#internPic').attr('src', 'images/people/JJS_pic.jpg');
+  // $('#internPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/JJS_pic.jpg');
+  $('#internContent>h1').html('Jiseung Jung');
+});
 
 //Alumni
-$('#LCM').on('click', function() {
-    $('#alumniPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/LCM_pic.jpg');
-    // $('#alumniPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/LCM_pic.jpg');
-    $('#alumniContent>h1').html('Changmin Lee');
-})
+$('#LCM').on('click', function () {
+  $('#alumniPic').attr('src', 'images/people/LCM_pic.jpg');
+  // $('#alumniPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/LCM_pic.jpg');
+  $('#alumniContent>h1').html('Changmin Lee');
+});
 
-$('#LWK').on('click', function() {
-    $('#alumniPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/LWK_pic.jpg');
-    // $('#alumniPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/LWK_pic.jpg');
-    $('#alumniContent>h1').html('Wonki Lee');
-})
+$('#LWK').on('click', function () {
+  $('#alumniPic').attr('src', 'images/people/LWK_pic.jpg');
+  // $('#alumniPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/LWK_pic.jpg');
+  $('#alumniContent>h1').html('Wonki Lee');
+});
 
-$('#LJW').on('click', function() {
-    $('#alumniPic').attr('src', '/home/jiseung/OneDrive/GitStorage/Biocybernetics-Web-App/people/LJW_pic.jpg');
-    // $('#alumniPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/LJW_pic.jpg');
-    $('#alumniContent>h1').html('Jiwon Lee');
-})
+$('#LJW').on('click', function () {
+  $('#alumniPic').attr('src', 'images/people/LJW_pic.jpg');
+  // $('#alumniPic').attr('src', 'file:///C:/Users/Pineapple/OneDrive%20-%20leverage%20proactive%20deliverables/GitStorage/Biocybernetics-Web-App/people/LJW_pic.jpg');
+  $('#alumniContent>h1').html('Jiwon Lee');
+});
